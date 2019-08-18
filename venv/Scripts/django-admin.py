294505af78c659c5756f990341fd224bc6642d73
antoniocassano84 <50021838+antoniocassano84@users.noreply.github.com>
2019-08-18 08:13:16 +0100

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\acpol_xljtyh9\PycharmProjects\ChallengeTvQuiz\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
